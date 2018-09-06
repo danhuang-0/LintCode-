cf308957ba052305af052f612d65216c8acf65a1
Facial01 <danhuang_0@163.com>
2018-09-06 23:12:14 +0800

    add 3

--- LintCode!/3. 统计数字.cpp
+++ LintCode!/3. 统计数字.cpp
@@ -1,44 +1,44 @@
-#include <iostream>
-
-using namespace std;
-
-class Solution {
-public:
-    /**
-     * @param k: An integer
-     * @param n: An integer
-     * @return: An integer denote the count of digit k in 1..n
-     */
-    int digitCounts(int k, int n) {
-        // write your code here
-		if( k==0 && n==0 )
-			return 1;
-
-		int nLeft = n /10 ;
-		int nMid = n % 10;
-		int nRight = 0;
-		int nDigit = 1;
-		int nCount = 0;
- 		while( nMid != 0 ) {
-			if( nMid < k ) {
-				nCount += nLeft * nDigit;
-			} else if ( nMid == k ) {
-				nCount += ( nLeft * nDigit + nRight + 1 );
-			} else {
-				nCount += ( nLeft + 1 ) * nDigit;
-			}
-			nDigit *= 10;
-			nRight = n % nDigit;
-			nLeft = n / nDigit / 10 ;
-			nMid = n / nDigit % 10;
-		}
-		return nCount;
-	}
-};
-
-int main (){
-
-	Solution s;
-	s.digitCounts(1, 12);
-	return 0;
-}
+//#include <iostream>
+//
+//using namespace std;
+//
+//class Solution {
+//public:
+//    /**
+//     * @param k: An integer
+//     * @param n: An integer
+//     * @return: An integer denote the count of digit k in 1..n
+//     */
+//    int digitCounts(int k, int n) {
+//        // write your code here
+//		if( n<10 )
+//			return 1;
+//		int nLeft = n /10 ;
+//		int nMid = n % 10;
+//		int nRight = 0;
+//		int nDigit = 1;
+//		int nCount = 0;
+// 		while( nMid != 0 || nLeft != 0 ) {
+//			if( nMid < k ) {
+//				nCount += nLeft * nDigit;
+//			} else if ( nMid == k ) {
+//				nCount += ( nLeft * nDigit + nRight + 1 );
+//			} else {
+//				if( k != 0 || nLeft != 0 )
+//					nCount += ( nLeft + 1 ) * nDigit;
+//			}
+//			nDigit *= 10;
+//			nRight = n % nDigit;
+//			nLeft = n / nDigit / 10 ;
+//			nMid = n / nDigit % 10;
+//		}
+//		return nCount;
+//	}
+//};
+//
+//int main (){
+//
+//	Solution s;
+//	s.digitCounts(0, 19);
+//	return 0;
+//}
--- LintCode!/51. 上一个排列.cpp
+++ LintCode!/51. 上一个排列.cpp
@@ -1,33 +1,50 @@
-//#include <iostream>
-//#include <vector>
-//
-//using namespace std;
-//
-//class Solution {
-//public:
-//    /*
-//     * @param nums: A list of integers
-//     * @return: A list of integers that's previous permuation
-//     */
-//    vector<int> previousPermuation(vector<int> &nums) {
-//        // write your code here
-// 
-//		bool isFind = false;
-//
-//		for( auto ite=nums.rbegin(); ite!=nums.rend(); ite++ ){
-//			for( auto )
-//		}
-//
-//
-//    }
-//};
-//
-//int main (){
-//
-//	int arr1[] = {1,3,2,3};
-//	int arr2[] = { 1,2,3,4 };
-//
-//	
-//
-//	return 0;
-//}
+// 1 3 2 4 5 6 -> 1 2 3 6 5 4
+// 1 2 3 6 5 4 -> 1 2 3 6 4 5
+// 1 2 3 6 4 5 -> 1 2 3 4 6 5
+// 按照字典序的上一个排列
+// 从后向前扫描，找到第一个升序的位置
+// 交换这两个位置，把后续全部翻转
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    /*
+     * @param nums: A list of integers
+     * @return: A list of integers that's previous permuation
+     */
+    vector<int> previousPermuation(vector<int> &nums) {
+        // write your code here
+		int index;
+		for( index = nums.size()-2; index >= 0; index-- ){
+			if( nums[index+1] < nums[index] )
+				break;
+		}
+		// 没找到
+		if( index == -1 ){
+			reverse( nums.begin(), nums.end() );
+		} else {
+			for( int index2 = nums.size()-1; ; index2-- ){
+				if( nums[index2] < nums[index] ){
+					swap( nums[index2], nums[index]);
+					reverse( find(nums.begin(), nums.end(), nums[index2]), nums.end() );
+					break;
+				}
+			}
+		}
+		return nums;
+    }
+};
+
+int main (){
+	
+	int arr[] = {2,1,1};
+	vector<int> nums( begin(arr), end(arr));
+	Solution s;
+	s.previousPermuation( nums );
+
+	return 0;
+}
