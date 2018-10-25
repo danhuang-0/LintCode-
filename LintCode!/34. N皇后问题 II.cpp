27cc9ad149905d5708175e5a929a591a878abef5
Facial01 <danhuang_0@163.com>
2018-10-25 20:01:15 +0800

    add 33, 34

--- LintCode!/34. N皇后问题 II.cpp
+++ LintCode!/34. N皇后问题 II.cpp
@@ -1,9 +1,4 @@
-//// 像剑指 Offer 里面一样无脑求全排列会无情的超时
-//// 基本思路就是用一个一位数组标示排列
-//// 其中数组下标表示行号，数组值表示列号，开始进行初始化保证不重复
-//// 用额外两个数组表示主对角线以及副对角线
-//// 利用同一主对角线行列差值相同和同一副对角线和值相同的特性
-//// 需要注意的是，4*4 的棋盘有 7 条主对角线，而不是四条
+//// 解法同33，只不过用一个全局变量进行计数罢了
 //
 //#include <iostream>
 //#include <vector>
@@ -19,14 +14,11 @@
 //     * @return: All distinct solutions
 //     */
 //	int m_nCountOfLine;
-//	void DFS(vector<vector<string>> &vecRet, vector<int> &vecNums, int nLevel
+//	int m_nCountOfAnswer;
+//	void DFS( vector<int> &vecNums, int nLevel
 //		, vector<bool> &vecMajor, vector<bool> &vecMinor ){
 //		if( nLevel == vecNums.size() ){
-//			vector<string> vecTemp(vecNums.size(), string(vecNums.size(), '.'));
-//			for( int i=0; i<vecNums.size(); i++ ){
-//				vecTemp[i][vecNums[i]] = 'Q';
-//			}
-//			vecRet.push_back( vecTemp );
+//			m_nCountOfAnswer ++;
 //			return;
 //		}
 //		for( int i=nLevel; i<vecNums.size(); i++ ){
@@ -40,7 +32,7 @@
 //				vecMajor[ ( (nRow - nCol) + m_nCountOfLine ) % m_nCountOfLine ] = true;
 //				vecMinor[ nRow + nCol ] = true;
 //
-//				DFS( vecRet, vecNums, nLevel+1, vecMajor, vecMinor );
+//				DFS( vecNums, nLevel+1, vecMajor, vecMinor );
 //				
 //				vecMajor[ ( (nRow - nCol) + m_nCountOfLine ) % m_nCountOfLine ] = false;
 //				vecMinor[ nRow + nCol ] = false;
@@ -49,25 +41,27 @@
 //		}
 //	}
 //
-//    vector<vector<string>> solveNQueens(int n) {
+//	int totalNQueens(int n) {
 //        // write your code here
-//		vector<vector<string>> vecRet;
+//
 //		vector<int> vecNums;
 //		m_nCountOfLine = n*2-1;
+//		m_nCountOfAnswer = 0;
 //		vector<bool> vecMajor( m_nCountOfLine, false );
 //		vector<bool> vecMinor( m_nCountOfLine, false );
 //
 //		for( int i=0; i<n; i++ ){
 //			vecNums.push_back(i);	
 //		}
-//		DFS( vecRet, vecNums, 0, vecMajor, vecMinor);
-//		return vecRet; 
+//
+//		DFS( vecNums, 0, vecMajor, vecMinor);
+//		return m_nCountOfAnswer;
 //    }
 //};
 //
 //int main (){
 //
 //	Solution s;
-//	s.solveNQueens( 8 );
+//	s.totalNQueens( 8 );
 //	return 0;
 //}
