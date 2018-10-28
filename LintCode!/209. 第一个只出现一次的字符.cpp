e4ed4e7d1b4b0029e3d0f78a709e42162cedaec6
Facial01 <danhuang_0@163.com>
2018-10-28 23:59:38 +0800

    add 209

---
+++ LintCode!/209. 第一个只出现一次的字符.cpp
@@ -0,0 +1,44 @@
+// 不使用额外空间
+// O（n^2）的算法就好了
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    /**
+     * @param str: str: the given string
+     * @return: char: the first unique character in a given string
+     */
+    char firstUniqChar(string &str) {
+        // Write your code here
+		char chRet = 0;
+		
+		for( int i=0; i<str.size(); i++ ){
+			bool isFind = false;
+			for( int j=0; j<str.size(); j++ ){
+				if( j == i )
+					continue;
+				if( str[i] == str[j] ){
+					isFind = true;
+					break;
+				}
+			}
+			if( !isFind ){
+				chRet = str[i];
+				break;
+			}
+		}
+		return chRet;
+    }
+};
+
+int main(){
+
+	string str("abaccdeff");
+	Solution s;
+	s.firstUniqChar(str);
+
+	return 0;
+}
