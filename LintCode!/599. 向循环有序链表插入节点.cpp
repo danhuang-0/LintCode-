b7c7d8cb80f9b19151c4b39f1f4d6496b096a01c
Facial01 <danhuang_0@163.com>
2019-01-02 22:47:35 +0800

    add 599

--- LintCode!/599. 向循环有序链表插入节点.cpp
+++ LintCode!/599. 向循环有序链表插入节点.cpp
@@ -1,4 +1,10 @@
+//// 只要找对插入位置就好
+//// 需要判断传入是否为空，是否一个节点
+//// 是否全是一个值
+//// 然后额外注意，当前值有可能比所有的值都大或者都小
+//
 //#include <iostream>
+//#include <vector>
 //
 //using namespace std;
 //
@@ -25,12 +31,43 @@
 //		if( node == nullptr ){
 //			pNode->next = pNode;
 //			return pNode;
+//		} else if ( node->next == node ){
+//			pNode->next = node->next;
+//			node->next = pNode;
+//			return node;
 //		}
-//		
+//		ListNode * pLeft = node;
+//		ListNode * pRight = node->next;
+//
+//		while( !(pLeft->val <= x && pRight->val > x) ){
+//			pLeft = pRight;
+//			pRight = pRight->next;
+//			// 循环
+//			if( pLeft == node )
+//				break;
+//			// 头尾交接处
+//			if ( pLeft->val > pRight->val ){
+//				if( x > pLeft->val )
+//					break;
+//				if( x < pRight->val )
+//					break;
+//			}
+//		}
+//		pLeft->next = pNode;
+//		pNode->next = pRight;
+//		return node;
 //    }
 //};
 //
 //int main(){
 //
+//	int arr[] = {3,5,1,2};
+//	vector<ListNode> vecNodes( begin(arr), end(arr));
+//	for( int i=0; i<vecNodes.size()-1; i++ )
+//		vecNodes[i].next = & vecNodes[i+1];
+//	vecNodes[3].next = &vecNodes[0];
+//	Solution s;
+//	s.insert( &vecNodes[0], 7);
+//	 
 //	return 0;
 //}
